50c8a46e3333f52580853aa7680dfc1e33b8f76d
hotline404 <cosmoyj7733@gmail.com>
2024-06-12 13:58:49 +0900

    Feat : 헤더 푸터 추가

--- grid_animation/common/header-footer.js
+++ grid_animation/common/header-footer.js
@@ -1,4 +1,7 @@
-const customHeader = document.querySelector(".header-container")
+import createToggle from "../switch/toggleSwitch";
+
+const customHeader = document.querySelector(".header-container");
+const customFooter = document.querySelector(".footer-container");
 
 function createHeader () {
   const headerContents = document.createElement("header");
@@ -6,8 +9,34 @@ function createHeader () {
   headerContents.innerHTML = `
     <div class="header-contents">
       <h1 class="title">GRID PROJECT</h1>
+      <div class="toggle-container"></div>
     </div>
-  `
+  `;
 
   customHeader.appendChild(headerContents);
+
+  const toggleContainer = document.querySelector(".toggle-container");
+  
+  toggleContainer.appendChild(createToggle());
+
 }
+
+function createFooter () {
+  const footerContents = document.createElement("footer");
+
+  footerContents.innerHTML = `
+    <div class="footer-contents">
+      <p>제작 : </p>
+      <span>김영준(hotline404)</span>
+      <p>email : </p>
+      <span>cosmoyj7733@gmail.com</span>
+    </div>
+  `;
+
+  customFooter.appendChild(footerContents);
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  createHeader();
+  createFooter();
+})
--- grid_animation/switch/toggleSwitch.js
+++ grid_animation/switch/toggleSwitch.js
@@ -1,14 +1,14 @@
-const toggleContainer = document.querySelector(".toggle-container")
-
 function createToggle () {
   const toggleSwitch = document.createElement("div");
-
+  
   toggleSwitch.innerHTML = `
     <label>
       <input role="switch" type="checkbox" />
       <span>모드</span>
     </label>
-  `
+  `;
 
-  toggleContainer.appendChild(toggleSwitch);
+  return toggleSwitch;
 }
+
+export default createToggle;
