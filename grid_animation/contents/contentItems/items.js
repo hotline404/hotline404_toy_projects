cc3e96614b685cebdc6271fadb3741f2fe3e45da
hotline404 <cosmoyj7733@gmail.com>
2024-06-18 16:34:56 +0900

    Feat: resume 추가

--- grid_animation/contents/contentItems/items.js
+++ grid_animation/contents/contentItems/items.js
@@ -24,7 +24,23 @@ const gridItem3 = `
 
 `
 const gridItem4 = `
-<div class="resume">RESUME</div>
+<div class="resume">
+  <h5>프론트 엔드 개발자 김영준 이력서</h5>
+  <p class="intro">간단한 자기소개</p>
+  <div class="box">
+    <p>Node. JS 환경에서 React JS, Axios, styled-components, redux-react를 다루는 개발자</p>
+<p class="title">[새로운 시도를 좋아하고 배우는 것에 두려움이 없다]</p>
+   <p class="dec">&ensp;- 데이터를 다루는 대회에서 코딩을 공부하고 대회에서 적용해 본 경험과 해당 대회에서 최우수상 수상과 대학 졸업 논문이라는 결과 </p>
+    <p class="dec">&ensp;- 웹의 성능 향상을 위해 클린 아키텍쳐와 다양한 프론트 엔드 디자인 패턴을 학습하고 적용</p>
+    <p class="dec">&ensp;- 현재는 CORS정책, mixed contents를 대처하기 위해 프록시 서버를 학습</p>
+
+<p class="title">[“왜?”라는 질문을 던지며 개념을 파고드는 물리학적 사고방식을 가짐]</p>
+<p class="dec">&ensp;- 컨벤션을 중요하게 생각하고 또 다른 방법을 찾기 위한 개념을 파고드는 공부를 통해 두 번의 프로젝트를 완성</p>
+
+<p class="title">[재활용성에 집착하는 개발자]</p>
+<p class="dec">&ensp;- stateful, Container, Presentational 등 다양한 패턴에 대해 알아가고 또 적용하는 것을 좋아함</p>
+  </div>
+</div>
 `
 
 export const GRID = {
